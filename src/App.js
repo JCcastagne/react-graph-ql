7e470b8deea5ff07c32076f4dfa7ebc4a6c1d177
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2022-05-07 01:08:35 -0400

    Complete building fetching functionality.
    Now fetches data and displays it as a list. Error handling included.

--- src/App.js
+++ src/App.js
@@ -3,32 +3,51 @@ import './App.css'
 
 let endpoint = `https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3`
 
-let graphQuery = `
+function App () {
+  // useEffect(() => {}, [])
+  const [chartData, setChartData] = useState()
+  const [contractAddress, setContractAddress] = useState(
+    '0x8ad599c3a0ff1de082011efddc58f1908eb6e6d8'
+  )
+
+  let graphQuery = `
 {
-  pool(id: "0x8ad599c3a0ff1de082011efddc58f1908eb6e6d8") {
-    poolHourData(orderBy: periodStartUnix) {
+  pool(id: "${contractAddress}") {
+    poolHourData(orderBy: periodStartUnix, orderDirection: desc) {
       token0Price
       token1Price
       periodStartUnix
     }
+    token0 {
+      symbol
+    }
+    token1 {
+      symbol
+    }
   }
 }
-`
 
-function App () {
-  // useEffect(() => {}, [])
-  const [chartData, setChartData] = useState()
+  `
 
   function getData () {
     console.log('received clicked')
+
     fetch(endpoint, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ query: graphQuery })
     })
       .then(response => response.json())
-      .then(data => setChartData(data))
-      .catch(err => console.log(err))
+      .then(data => {
+        if (!data.pool) {
+          alert('Please enter a valid pool/pair address')
+        } else {
+          setChartData(data)
+        }
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }
 
   function unixTimeConverter (unixTime) {
@@ -40,11 +59,40 @@ function App () {
   //   chartData && console.log(chartData.data.pool.poolHourData)
   // }, [chartData])
 
+  // useEffect(() => {
+  //   console.log(graphQuery)
+  // }, [graphQuery])
+
   return (
     <div className='App'>
-      <div className='button' onClick={() => getData()}>
-        <p>Get data</p>
-      </div>
+      <h1>Get the price history of a pool</h1>
+
+      <form>
+        <label>Enter a valid pool address</label>
+        <input
+          type='text'
+          name='poolAddress'
+          onChange={e => {
+            setContractAddress(e.target.value.toLowerCase())
+          }}
+        />
+
+        <button
+          className='button'
+          onClick={e => {
+            e.preventDefault()
+            getData()
+          }}
+        >
+          <p>Get data</p>
+        </button>
+      </form>
+
+      <h2>
+        {chartData &&
+          `${chartData && chartData.data.pool.token0.symbol} VS ${chartData &&
+            chartData.data.pool.token1.symbol}`}
+      </h2>
 
       <ul>
         {chartData &&
